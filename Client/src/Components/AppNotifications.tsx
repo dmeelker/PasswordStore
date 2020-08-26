229431b4d5672cc179ec7fcbf0ec41f72d8b2147
Dennis Meelker <dennis.meelker@infi.nl>
2020-08-26 08:11:35 +0200

    Center notification

--- Client/src/Components/AppNotifications.tsx
+++ Client/src/Components/AppNotifications.tsx
@@ -6,7 +6,7 @@ export function AppNotifications() {
   const notifications = useObservable(NotificationService.notifications);
 
   if (notifications.length > 0) {
-    return <div className="fixed --transform-translate-x" style={{ left: "50%" }}>
+    return <div className="fixed" style={{ left: "50%", transform: "translate(-50%, 0)" }}>
       {notifications.map((notification) => <div key={notification.id} className="bg-green-400 border-b-2 border-green-600 px-4 py-2 rounded shadow mb-1 text-sm text-center">{notification.message}</div>)}
     </div>;
   }
